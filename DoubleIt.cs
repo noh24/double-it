4bc46c5d402df3971c71570a01975f074d674d74
Brian Noh <noh24@ymail.com>
2022-12-02 19:35:03 -0500

    add triple number method

--- DoubleIt.cs
+++ DoubleIt.cs
@@ -4,11 +4,14 @@ class DoubleIt
 {
   static void Main()
   {
-    Console.WriteLine("Give me a number and I will double it for you!");
+    Console.WriteLine("Give me a number and I will double and triple it for you!");
     string stringNumber = Console.ReadLine();
     int yourNumber = int.Parse(stringNumber);
     int yourDoubledNumber = yourNumber * 2;
     string stringDoubledNumber = yourDoubledNumber.ToString();
     Console.WriteLine("I doubled your number for you: " + stringDoubledNumber);
+    int yourTripledNumber = yourNumber * 3;
+    string stringTripledNumber = yourTripledNumber.ToString();
+    Console.WriteLine("I tripled your number for you: " + yourTripledNumber);
   }
 }
